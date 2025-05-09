6e6fdaf2b1c5b565a00aef0615e865c253923f58
Kalyani <kalyani.jetcode@gmail.com>
2025-05-09 14:37:16 +0800

    initial commit

--- src/main/java/com/jetcode/su57/RegistryApplication.java
+++ src/main/java/com/jetcode/su57/RegistryApplication.java
@@ -2,12 +2,14 @@ package com.jetcode.su57;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class Su57Application {
+public class RegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Su57Application.class, args);
+		SpringApplication.run(RegistryApplication.class, args);
 	}
 
 }
--- src/test/java/com/jetcode/su57/RegistryApplicationTests.java
+++ src/test/java/com/jetcode/su57/RegistryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Su57ApplicationTests {
+class RegistryApplicationTests {
 
 	@Test
 	void contextLoads() {
