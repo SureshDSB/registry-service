0ad10378932f9c450020b367f63176dd6bbd7d23
Kalyani <kalyani.jetcode@gmail.com>
2025-05-09 14:01:19 +0800

    initial commit

---
+++ src/test/java/com/jetcode/su57/Su57ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetcode.su57;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Su57ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
